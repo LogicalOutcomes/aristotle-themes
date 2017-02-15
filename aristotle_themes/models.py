0bfd6509c0473e3dd7de2489841c84f4af95750c
rafen <capdevielle@gmail.com>
2017-02-15 12:33:59 -0300

    Changed settings for theme apps and set FAVICON option

--- aristotle_themes/models.py
+++ aristotle_themes/models.py
@@ -5,8 +5,12 @@ DEFAULT_THEME = 'bootstrap-material-design'
 THEMES = {
     'bootstrap-material-design': {
         'css_files': [
-            'bootstrap-material-design/dist/css/bootstrap-material-design.min.css',
-            'bootstrap-material-design/dist/css/ripples.min.css',
+            'node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.min.css',
+            'node_modules/bootstrap-material-design/dist/css/ripples.min.css',
+        ],
+        'js_files': [
+            'node_modules/bootstrap-material-design/dist/js/material.min.js',
+            'node_modules/bootstrap-material-design/dist/js/ripples.min.js',
         ]
     }
 }
--- aristotle_themes/templatetags/aristotle_themes.py
+++ aristotle_themes/templatetags/aristotle_themes.py
@@ -4,6 +4,8 @@ from ..models import THEMES, DEFAULT_THEME
 
 register = template.Library()
 
+config = getattr(settings, 'ARISTOTLE_SETTINGS', {})
+
 
 @register.assignment_tag
 def get_main_scss_file():
@@ -11,25 +13,27 @@ def get_main_scss_file():
     Get main scss file from django settings
     Usage: {% get_main_scss_file as main_scss %}
     """
-    return getattr(
-        settings,
-        'ARISTOTLE_THEMES_SCSS',
+    return config.get(
+        'THEMES_MAIN_SCSS',
         'scss/aristotle_theme.scss'
     )
 
 
 @register.assignment_tag
-def get_themes_css_files():
+def get_theme():
     """
     Get a list of css files to include
-    Usage: {% get_themes_css_files as css_files %}
+    Usage: {% get_theme as theme %}
     """
-    theme_name = getattr(
-        settings,
-        'ARISTOTLE_THEMES_NAME',
+    theme_name = config.get(
+        'THEMES_NAME',
         DEFAULT_THEME
     )
-    if theme_name in THEMES:
-        return THEMES[theme_name]['css_files']
+    themes = config.get(
+        'THEMES_LIST',
+        THEMES,
+    )
+    if theme_name in themes:
+        return themes[theme_name]
     else:
-        return []
+        return {}
